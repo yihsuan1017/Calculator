21295a8316aefc21ad0c9373d0d8264aacb1e5dd
yihsuan1017 <tasi.peter062@gmail.com>
2023-05-08 14:40:47 +0800

    5/7增加百分鍵與倒退鍵
    運用程式碼

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,224 +20,156 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        float firstNumber, secondNumber; // firstNumber 儲存第一個數字，secondNumber 儲存第二個數字
+        int operators = -1; // 記錄選擇哪一種運算符號？0:加、1:減、2:乘、3:除、-1:重新設定
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_KeyUp(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Button_KeyUp_1(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Button_KeyUp_2(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Button_KeyUp_3(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Button_KeyUp_4(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Button_KeyUp_5(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Button_KeyUp_6(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Button_KeyUp_7(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Button_KeyUp_8(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Button_KeyUp_9(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Button_KeyUp_10(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Button_KeyUp_11(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Button_KeyUp_12(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Button_KeyUp_13(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Button_KeyUp_14(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Grid_KeyUp(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Button_KeyUp_15(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Button_KeyUp_16(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Button_KeyUp_17(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Button_KeyUp_18(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void btnClear_KeyUp(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void btnBack_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnClear_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnPC_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnSeven_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnFour_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void btnOne_Click(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void btnDot_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnEight_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnFive_Click(object sender, RoutedEventArgs e)
-        {
-
+            Add_Number("1");
         }
 
         private void btnTwo_Click(object sender, RoutedEventArgs e)
         {
-
+            Add_Number("2");
         }
 
-        private void btnZero_Click(object sender, RoutedEventArgs e)
+        private void btnThree_Click(object sender, RoutedEventArgs e)
         {
-
+            Add_Number("3");
         }
 
-        private void btnNine_Click(object sender, RoutedEventArgs e)
+        private void btnFour_Click(object sender, RoutedEventArgs e)
         {
+            Add_Number("4");
+        }
 
+        private void btnFive_Click(object sender, RoutedEventArgs e)
+        {
+            Add_Number("5");
         }
 
         private void btnSix_Click(object sender, RoutedEventArgs e)
         {
+            Add_Number("6");
+        }
+
+        private void btnSeven_Click(object sender, RoutedEventArgs e)
+        {
+            Add_Number("7");
+        }
+
+        private void btnEight_Click(object sender, RoutedEventArgs e)
+        {
+            Add_Number("8");
+        }
+
+        private void btnNine_Click(object sender, RoutedEventArgs e)
+        {
+            Add_Number("9");
+        }
+
+        private void btnZero_Click(object sender, RoutedEventArgs e)
+        {
+            Add_Number("0");
+        }
+
+        private void Add_Number(string _number)
+        {
+            if (txtNumber.Text == "0")
+                txtNumber.Text = "";
+            txtNumber.Text = txtNumber.Text + _number;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Select_Operator(0);
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            Select_Operator(1);
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            Select_Operator(2);
+        }
+
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            Select_Operator(3);
+        }
+
+        private void Select_Operator(int _operator)
+        {
+            firstNumber = Convert.ToSingle(txtNumber.Text); //將輸入文字框轉換成浮點數，存入第一個數字的全域變數
+            txtNumber.Text = "0"; //重新將輸入文字框重新設定為0
+            operators = _operator; //選擇「加」號
+        }
+
+        private void btnDot_Click(object sender, RoutedEventArgs e)
+        {
+            // 確認輸入文字框中完全沒有小數點
+            if (txtNumber.Text.IndexOf(".") == -1)
+                txtNumber.Text = txtNumber.Text + ".";
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtNumber.Text = "0";
+            firstNumber = 0f;
+            secondNumber = 0f;
+            operators = -1;
+        }
+
+        private void btnBACK_Click(object sender, RoutedEventArgs e)
+        {
 
         }
 
-        private void btnThree_Click(object sender, RoutedEventArgs e)
+        private void btnBack_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPc_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
         private void btnEqual_Click(object sender, RoutedEventArgs e)
         {
+            float finalResults = 0f; //宣告最後計算結果變數
+            secondNumber = Convert.ToSingle(txtNumber.Text); //將輸入文字框轉換成浮點數，存入第二個數字的全域變數
 
-        }
+            //依照四則運算符號的選擇，進行加減乘除
+            switch (operators)
+            {
+                case 0:
+                    finalResults = firstNumber + secondNumber;
+                    break;
+                case 1:
+                    finalResults = firstNumber - secondNumber;
+                    break;
+                case 2:
+                    finalResults = firstNumber * secondNumber;
+                    break;
+                case 3:
+                    finalResults = firstNumber / secondNumber;
+                    break;
+            }
 
-        private void btnDivide_Click(object sender, RoutedEventArgs e)
-        {
+            txtNumber.Text = string.Format("{0:0.##########}", finalResults); //在輸入文字框中，顯示最後計算結果，並且轉換成格式化的字串內容
 
-        }
-
-        private void btnPlus_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnAdd_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnMinus_Click(object sender, RoutedEventArgs e)
-        {
-
+            //重置所有全域變數
+            firstNumber = 0f;
+            secondNumber = 0f;
+            operators = -1;
         }
     }
 }
