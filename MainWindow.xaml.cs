1230ce3e0f475fb5604a1fde830a6242bc1c32a8
yihsuan1017 <tasi.peter062@gmail.com>
2023-05-08 14:06:58 +0800

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_KeyUp_1(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_KeyUp_2(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_KeyUp_3(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_KeyUp_4(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_KeyUp_5(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_KeyUp_6(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_KeyUp_7(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_KeyUp_8(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_KeyUp_9(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_KeyUp_10(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_KeyUp_11(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_KeyUp_12(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_KeyUp_13(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_KeyUp_14(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Grid_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_KeyUp_15(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_KeyUp_16(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_KeyUp_17(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_KeyUp_18(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void btnClear_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPC_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSeven_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnFour_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnOne_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDot_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEight_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnFive_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnTwo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnZero_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnNine_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSix_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnThree_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEqual_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
